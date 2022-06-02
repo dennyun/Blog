3945783139ed70393b7ddbb5f564ad082983a7ce
denilson <denilson.silvastt@gmail.com>
2022-06-01 23:04:05 -0300

    inicial home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,39 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React from 'react'
+import {View, Text, StyleSheet, TouchableOpacity , SafeAreaView} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
 
 export default function Home() {
+    const navigation = useNavigation();
+
     return(
-        <View style={styles.container}>
-            <Text style={styles.title}>HOME</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Class Lock</Text>
+                <TouchableOpacity>
+                    <Feather name='search' size={24} color='#FFF'/>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#232630'
+    },
+    header:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 18,
+        marginTop: 18,
+        marginBottom: 24
     },
     title:{
-        color: '#000',
-        fontSize: 22
-    }
+        fontSize: 28,
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
 })
